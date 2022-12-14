03df9a2859a23f9460eb97f40be39f0f1d0ef2f3
Daniel Svensson <daniel.svensson682@gmail.com>
2022-12-14 02:58:59 +0100

    implemented first segment of SQL queries to get Employees

--- Labb3-CallDatabase/Data/SQLCalls.cs
+++ Labb3-CallDatabase/Data/SQLCalls.cs
@@ -1,18 +1,135 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Labb3_CallDatabase.Data
 {
     internal class SQLCalls
     {
-        public void GetPersonell(SqlConnection conn)
+        SqlCommand cmd;
+        SqlConnection conn = new("Data Source=GREENMECHANOID;" + "Initial Catalog=Labb2-SchoolDatabase;" + "Trusted_Connection=true");
+        public SQLCalls()
         {
+            cmd = new()
+            {
+                CommandTimeout = 60,// if command is endless or unresponsive, it will abort after 60sec
+                Connection = conn,
+                CommandType = CommandType.Text
+            };
+        }
+        public void GetEmployees()
+        {
+            int Selection = 0; bool CorrectSelection = false, keepLooping = true;
+            string SQLCountString, SQLQuery;
 
+            do
+            {
+                switch (Selection)
+                {
+                    default:
+                        do
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Do you wish to see all employees or just a certain type of employee?");
+                            Console.WriteLine("1: All Employees");
+                            Console.WriteLine("2: Only Teachers");
+                            Console.WriteLine("3: Only secretaries");
+                            Console.WriteLine("4: Only Principals");
+                            Console.WriteLine("5: Only Janitors");
+                            Console.WriteLine("6: Only Cafeteria Staff");
+                            Console.WriteLine("9: Return to last menu");
+                            int.TryParse(Console.ReadLine(), out Selection);
+                            if (Selection == 1 || Selection == 2 || Selection == 3 ||
+                            Selection == 4 || Selection == 5 || Selection == 6 || Selection == 9)
+                            {
+                                CorrectSelection = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Wrong input, Try again");
+                            }
+                        } while (!CorrectSelection);
+                        break;
+                    case 1:
+                        SQLCountString = "SELECT Count(*) FROM Staff";
+                        SQLQuery = "SELECT StaffID, Fullname, Occupation FROM Staff ORDER BY StaffID";
+                        TryConnection(SQLCountString, SQLQuery);
+                        Thread.Sleep(5000);
+                        goto default;
+                    case 2:
+                        SQLCountString = "SELECT Count(*) FROM Staff Where Occupation = 'Teacher'";
+                        SQLQuery = "SELECT StaffID, Fullname, Occupation FROM Staff Where Occupation = 'Teacher' ORDER BY StaffID";
+                        TryConnection(SQLCountString, SQLQuery);
+                        Thread.Sleep(5000);
+                        goto default;
+                    case 3:
+                        SQLCountString = "SELECT Count(*) FROM Staff Where Occupation = 'Secretary'";
+                        SQLQuery = "SELECT StaffID, Fullname, Occupation FROM Staff Where Occupation = 'Secretary' ORDER BY StaffID";
+                        TryConnection(SQLCountString, SQLQuery);
+                        Thread.Sleep(5000);
+                        goto default;
+                    case 4:
+                        SQLCountString = "SELECT Count(*) FROM Staff Where Occupation = 'Principal'";
+                        SQLQuery = "SELECT StaffID, Fullname, Occupation FROM Staff Where Occupation = 'Principal' ORDER BY StaffID";
+                        TryConnection(SQLCountString, SQLQuery);
+                        Thread.Sleep(5000);
+                        goto default;
+                    case 5:
+                        SQLCountString = "SELECT Count(*) FROM Staff Where Occupation = 'Janitor'";
+                        SQLQuery = "SELECT StaffID, Fullname, Occupation FROM Staff Where Occupation = 'Janitor' ORDER BY StaffID";
+                        TryConnection(SQLCountString, SQLQuery);
+                        Thread.Sleep(5000);
+                        goto default;
+                    case 6:
+                        SQLCountString = "SELECT Count(*) FROM Staff Where Occupation LIKE '%Cafeteria%'";
+                        SQLQuery = "SELECT StaffID, Fullname, Occupation FROM Staff Where Occupation LIKE '%Cafeteria%' ORDER BY StaffID";
+                        TryConnection(SQLCountString, SQLQuery);
+                        Thread.Sleep(5000);
+                        goto default;
+                    case 9:
+                        Console.Clear();
+                        keepLooping = false;
+                        break;
+                }
+            } while (keepLooping);
+
+        }
+
+        public void TryConnection(string SQLCountString, string SQLDataSelection)
+        {
+            SqlDataReader dr;
+            try
+            {
+                cmd.CommandText = SQLCountString;
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                {
+                    object objCount = cmd.ExecuteScalar();
+                    int iCount = (int)objCount;
+                    cmd.CommandText = SQLDataSelection;
+                    dr = cmd.ExecuteReader(CommandBehavior.SingleResult);
+                    // For loop to read everything from the table  
+                    for (int i = 0; i < iCount; i++)
+                    {
+                        dr.Read(); // Read one row from the table  
+                        Console.WriteLine("StaffID: {0}  Name: {1}  Occupation: {2}", dr[0], dr[1], dr[2]);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.Write(exp.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
--- Labb3-CallDatabase/Program.cs
+++ Labb3-CallDatabase/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Labb3_CallDatabase.Data;
+using Microsoft.Data.SqlClient;
 
 namespace Labb3_CallDatabase
 {
@@ -6,8 +7,9 @@ namespace Labb3_CallDatabase
     {
         static void Main(string[] args)
         {
-            SqlConnection MyCon = new SqlConnection("Data Source=GREENMECHANOID;" + "Initial Catalog=Labb2-SchoolDatabase;" + "Trusted_Connection=true");
-
+            
+            SQLCalls sql = new SQLCalls();
+            EFCalls efcalls = new EFCalls();
             bool keepLooping = true, correctInput = false;
             int navigation = 0;
             do
@@ -19,15 +21,16 @@ namespace Labb3_CallDatabase
                         navigation = 0; keepLooping = true; correctInput = false;
                         do
                         {
-                        Console.WriteLine("1: Listing of personell"); //SQL
-                        Console.WriteLine("2: Listing of students"); //Entity
+                        Console.WriteLine("Main Menu . Please choose a option");
+                        Console.WriteLine("1: Listing of Employees"); //SQL
+                        Console.WriteLine("2: Listing of Students"); //Entity
                         Console.WriteLine("3: Listing class of students");//Entity
                         Console.WriteLine("4: List Grades set previous month"); //SQL
                         Console.WriteLine("5: List Average grade in all classes"); //SQL
                         Console.WriteLine("6: Add new Student"); //SQL
-                        Console.WriteLine("7: Add new personell");//Entity
+                        Console.WriteLine("7: Add new Employee");//Entity
                         Console.WriteLine("9: Exit program.");
-                        int.TryParse(Console.ReadLine(), out navigation);
+                        int.TryParse(Console.ReadLine(), out navigation); // error checking on input, keeps looping until correct input
                         if (navigation == 1 || navigation == 2 || navigation == 3 ||
                             navigation == 4 || navigation == 5 || navigation == 6 ||
                             navigation == 7 || navigation == 9)
@@ -36,12 +39,15 @@ namespace Labb3_CallDatabase
                         }
                         else
                         {
+                                Console.Clear(); // clears to not flood the console with same info
                                 Console.WriteLine("Wrong input, Try again.");
                         }
 
                         } while (!correctInput);
                         break;
                     case 1:
+                        Console.Clear();
+                        sql.GetEmployees();
                         goto default;
                     case 2:
                         goto default;
@@ -61,7 +67,7 @@ namespace Labb3_CallDatabase
                 }
 
 
-            } while (keepLooping);
+            } while (keepLooping); // infinite loop until user wants to exit
         }
     }
 }
